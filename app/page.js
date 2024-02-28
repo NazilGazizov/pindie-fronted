c3b3cf7c04d36710fadeaf32b6a8245471e46a60
NazilGazizov <nazil.gazizov@mail.ru>
2024-02-28 17:25:44 +0300

    commit

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import { getGamesByCategory } from "./data/data-utils";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Banner } from "./components/Banner/Banner";
+import { Promo } from "./components/Promo/Promo";
+import CardList from "./components/CardsList/CardList";
+
+export default function Home() {
+
+  const popularGames = getGamesByCategory("popular");
+  const newGames = getGamesByCategory("new");
+
+  return (
+    <main className="main">
+    <Banner/>
+    <CardList id="popular" title="Популярное" data={popularGames}/>
+      <CardList id="new" title="Новинки" data={newGames}/>
+    <Promo/>
+  </main>
+  );
+}
